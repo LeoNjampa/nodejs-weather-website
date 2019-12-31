5004af90003b947e17113b060dbdbb958ee5945c
Leo Njampa <Leopold.Njampa@prudential.co.uk>
2019-12-31 01:05:03 +0000

    Removed ref to chalk pkg

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,12 @@
 const request = require('request')
-const chalk = require('chalk')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/a76a82395fe7eb088f022ba25a18c9aa/'+ latitude +',' + longitude + '?units=si'
     request({url, json: true}, (error, {body}) => {
         if (error) {
-            callback(chalk.red('Unable to connect to weather service!'), undefined)
+            callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
-            callback(chalk.red('Unable to find location!'), undefined)
+            callback('Unable to find location!', undefined)
         } else {
             const currChanceOfRain = body.currently.precipProbability
             const currTemp = body.currently.temperature
