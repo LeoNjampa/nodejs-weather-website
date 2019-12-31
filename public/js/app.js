fe3ce28c45313dde62fd7badd27167bbaa62d5b8
Leo Njampa <Leopold.Njampa@prudential.co.uk>
2019-12-31 00:43:07 +0000

    Made changes in readiness for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,18 +8,14 @@ weatherForm.addEventListener('submit', (event) => {
 
     const location = search.value
     
-    // console.log(location)
     messageOne.textContent='Loading...'
     messageTwo.textContent=''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                // console.log(data.error)
                 messageOne.textContent = data.error
             } else {
-                // console.log(data.location)
-                // console.log(data.forecast)
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
 
